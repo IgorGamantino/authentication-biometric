1d7a621d96ce92b8218fdeddafda59a975d0e4c4
Igor Amantino <justin.amantino@gmail.com>
2023-12-16 08:58:40 -0300

    create organization folders

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,12 @@
+
+import {View,Text} from "react-native"
+
+import { styles } from "./styles"
+export function Login () {
+  return (
+    
+    <View>
+      <Text>Ola mundo</Text>
+    </View>
+  )
+}
---
+++ src/screens/login/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f4fa",
+    alignItems: "center"
+    },
+})
