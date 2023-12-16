1dbd1cbeb6a016d3a2c2289b4f5ef7d32b23a7ce
Igor Amantino <justin.amantino@gmail.com>
2023-12-16 10:04:52 -0300

    add authentication local

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,12 +1,48 @@
 
-import {View,Text, ImageBackground} from "react-native"
+import {View,Text, ImageBackground, TextInput, TouchableOpacity} from "react-native"
 import backgroundImg from "../../assets/background.png"
 import { styles } from "./styles"
+
+
+import * as LocalAuthentication from "expo-local-authentication"
+import { useEffect } from "react";
 export function Login () {
+  
+
+async function verifyAvailableAuthentication() {
+    const isCompatible = await LocalAuthentication.hasHardwareAsync();
+
+    return isCompatible
+}
+
+
+ async function handleAuthentication() {
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Login",
+      fallbackLabel: "Authenticação falhou, tente com sua senha!"
+    })
+
+    if(auth){
+      console.log("logado")
+    }
+  
+ }
+
   return (
     
     <View style={styles.container}>
       <ImageBackground style={styles.img} source={backgroundImg} resizeMode="cover" />
+
+
+      <View style={styles.wrapperLogin}>
+          <Text style={styles.title}>Login</Text>
+         <TextInput placeholder="E-mail" placeholderTextColor="#808080" style={styles.input}/>
+         <TextInput  placeholder="Password"  placeholderTextColor="#808080" style={styles.input}/>
+
+         <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton} onPress={handleAuthentication}>Entrar</Text>
+         </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/login/styles.ts
+++ src/screens/login/styles.ts
@@ -1,14 +1,74 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // alignItems: "center"
     },
 
     img: {
       flex: 1,
       opacity: 0.80,
       backgroundColor: "#02bbff"
+    },
+
+    wrapperLogin: {
+      position: "absolute",
+      backgroundColor: "#fff",
+      width: 288,
+      height: 283,
+      left: 50,
+      right: 50,
+      top: 80,
+      borderRadius: 15,
+      paddingHorizontal: 20,
+
+      alignItems: "center",
+      justifyContent: "center",
+
+      borderTopWidth: 40,
+      borderColor: "#02bbff" 
+
+    },
+
+    title: {
+      position: "absolute",
+      top: -30,
+      left: 20,
+      color: "#FFF",
+      fontSize: 15,
+      fontWeight: "700"
+    },
+
+    input : {
+      width: "100%",
+      borderColor: "#E6E6E6",
+      height: 40,
+      paddingHorizontal: 10,
+      borderWidth: 1,
+      backgroundColor: "#F2F2F2",
+      borderRadius: 6,
+      marginBottom: 20,
+      color: "#808080",
+    },
+
+    button: {
+      backgroundColor: "#192E88",
+      width: "100%",
+      borderRadius: 15,
+      height: 40,
+    
+      alignItems: "center",
+      justifyContent: "center"
+      
+    },
+
+    textButton: {
+      color: "#ffff",
+      fontSize: 15
     }
+
+    
 })
